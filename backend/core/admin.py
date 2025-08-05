8eb325834e67e7089084fdf1b478bfcfcea02bbf
Piyushsukh <114552025+Piyushsukh@users.noreply.github.com>
2025-08-05 13:53:54 +0530

    hi

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseAdminSite
+from core.models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomAdminSite(BaseAdminSite):
+    pass
+
