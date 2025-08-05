d657a15603a1c07f2b89654182f4163a8323e3dc
Piyushsukh <114552025+Piyushsukh@users.noreply.github.com>
2025-08-05 17:26:33 +0530

    hii

--- backend/shop_app/settings.py
+++ backend/shop_app/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 AUTH_USER_MODEL = 'core.User'
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- backend/shop_app/urls.py
+++ backend/shop_app/urls.py
@@ -19,5 +19,11 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',views.RegisterView.as_view(), name='register'), 
+    path('login/',views.LoginView.as_view(), name='login'),
+    path('logout/',views.LogoutView.as_view(), name='logout'),
+    path('user/', views.UserView.as_view(), name='user'),
+    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('core/', include('core.urls')),
 ]
