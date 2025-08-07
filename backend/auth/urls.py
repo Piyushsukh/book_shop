f9b551a56ca29aecf484d25a31cbffcffbb7f753
Piyushsukh <114552025+Piyushsukh@users.noreply.github.com>
2025-08-07 13:36:22 +0530

    reset_password

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('login/',views.LoginView.as_view(), name='login'),
     path('logout/',views.LogoutView.as_view(), name='logout'),
     path('user/', views.UserView.as_view(), name='user'),
-    # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
 ]
--- backend/auth/views.py
+++ backend/auth/views.py
@@ -59,3 +59,22 @@ class UserView(APIView):
             "email":user.email,
             "is_active":user.is_active
         },status=status.HTTP_200_OK)
+    
+
+class PasswordResetView(APIView):
+    permission_classes=[IsAuthenticated]
+
+    def post(self,request):
+        user=request.user
+        old_password=request.data.get('old_password')
+        new_password=request.data.get('new_password')
+
+        if not user.check_password(old_password):
+            return Response({'error':'old password is incorrect'},status=status.HTTP_400_BAD_REQUEST)
+         
+        if not new_password:
+            return Response({'error':'New password is required'},status=status.HTTP_400_BAD_REQUEST)
+        
+        user.set_password(new_password)
+        user.save()
+        return Response({'message':'Password updated successfully'},status=status.HTTP_200_OK)
