38c0b2f0f8c960a9327fe5cad8908b1328eb1672
Piyushsukh <114552025+Piyushsukh@users.noreply.github.com>
2025-08-08 11:25:40 +0530

    book models

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name=models.CharField()
+
+class Author(models.Model):
+    name=models.CharField()
+
+class Publisher(models.Model):
+    name=models.CharField()
+
+class Book(models.Model):
+    name=models.CharField()
+    price=models.IntegerField()
+    discount=models.IntegerField()
+    image=models.ImageField()
+    preview=models.FileField()
+    last_sold=models.DateTimeField()
+    subject=models.ForeignKey(Subject,on_delete=models.PROTECT)
+    author=models.ForeignKey(Author,on_delete=models.PROTECT)
+    publisher=models.ForeignKey(Author,on_delete=models.PROTECT)
+
+
