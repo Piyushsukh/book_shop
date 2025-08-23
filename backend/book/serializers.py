6edfefd5caf2d8cb168b5c67db16b3eb08ad8b01
Piyushsukh <114552025+Piyushsukh@users.noreply.github.com>
2025-08-24 01:54:00 +0530

    hi

--- backend/book/serializers.py
+++ backend/book/serializers.py
@@ -5,6 +5,7 @@ class BookSerializer(serializers.ModelSerializer):
     author=serializers.StringRelatedField()
     publisher=serializers.StringRelatedField()
     subject=serializers.StringRelatedField()
+    image = serializers.ImageField(use_url=True)
     class Meta:
         model=Book
-        fields=('name','price','discount','author','publisher','image','preview','last_sold','subject','synopsis')
+        fields=('name','price','discount','author','publisher','image','preview','last_sold','publish_date','subject','synopsis')
